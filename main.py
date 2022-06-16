f70f0276ddff72105e92f4e9c1ed31dc01adb936
Nioobers <64842935+Nioobers@users.noreply.github.com>
2022-06-16 11:29:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main(markdown_file: str = typer.Argument(..., help = "The markdown file you 
         text = file.read() # get the content of the markdown file
         html = markdown.markdown(text) # convert the markdown in html
 
-    with open(html_name_file, "w") as file: # creat the html file
+    with open(html_name_file, "w") as file: # create the html file
         file.write(html) # write the html in the file
 
 if __name__ == "__main__":
