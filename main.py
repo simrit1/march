bc4573b569e92a144502862ec9e0ab5ee1a0bcf7
Nioobers <marceltoutdroitaubut@gmail.com>
2022-06-16 11:26:30 +0200

    Add comment

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = typer.Typer()
 
 @app.command(help = "An markdown to html converter.")
 def main(markdown_file: str = typer.Argument(..., help = "The markdown file you want to convert."), html_name_file: str = typer.Argument(..., help = "The name of the html file.")):
-    with open(markdown_file, "r") as file:
-        text = file.read()
-        html = markdown.markdown(text)
+    with open(markdown_file, "r") as file: # get the markdown file
+        text = file.read() # get the content of the markdown file
+        html = markdown.markdown(text) # convert the markdown in html
 
-    with open(html_name_file, "w") as file:
-        file.write(html)
+    with open(html_name_file, "w") as file: # creat the html file
+        file.write(html) # write the html in the file
 
 if __name__ == "__main__":
     app()
